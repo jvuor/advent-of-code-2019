a7109bada8e0fb6c2d60c1d2876cc5c5ea3ae464
Jussi Vuorinen <jussi.vuorinen@gmail.com>
2019-12-19 00:34:22 +0200

    solve day 9, part 2

--- src/day9/index.ts
+++ src/day9/index.ts
@@ -4,3 +4,5 @@ import { operate, parseIntcode } from './../shared/intcode-computer';
 const code = fs.readFileSync('src/day9/input.txt', { encoding: 'utf8' });
 const result = operate(parseIntcode(code), { inputs: [1], noOutputToConsole: true });
 console.log('Part 1 result:', result.output[0]);
+const result2 = operate(parseIntcode(code), { inputs: [2], noOutputToConsole: true });
+console.log('Part 2 result:', result2.output[0]);
